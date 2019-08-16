4a2363e9f4c85b16a95f68a35d114c72eae9ad49
Patrick Gordon <patrickgordon42@yahoo.com>
2019-08-16 15:01:36 -0500

    MVP

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { deleteSmurf } from '../actions/smurfAction'
 
 
-function Smurf ({smurf, deleteData}) {
+function Smurf(smurf, props) {
     // {console.log(smurf)}
     return(
         <section>
             <h4>HI I am {smurf.name}!</h4>
             <p>I am {smurf.age} years old and only {smurf.height} tall</p>
             <button onClick={(e) => {
-                e.preventDefault()
-                return deleteData(smurf.id)
+             props.deleteSmurf(smurf.id)
             }}>Delete the Smurf!</button>
         </section>
     )
 }
 
-export default Smurf
+export default connect(
+    null, {  deleteSmurf }
+  )(Smurf)
